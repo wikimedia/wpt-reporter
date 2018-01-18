30748f2163d95755a699e39d2ba5731a51413cb9
Peter Hedenskog <phedenskog@wikimedia.org>
2018-01-18 08:35:31 +0100

    Move Internet Explorer tests to own test files.
    By moving all IE tests to separate files, we can setup
    the new runs by only adding a extra job in Jenkins for
    the Linux agent.
    
    Bug: T175240
    Change-Id: I786ea6153a26364c9880b314826695094a022d71

--- test/rulethemall.sh
+++ test/rulethemall.sh
@@ -28,9 +28,11 @@ export WMF_WPT_LOCATION="us-west-1"
 node bin/index.js --batch scripts/batch/asia.txt
 node bin/index.js --batch scripts/batch/mobile.txt
 node bin/index.js --batch scripts/batch/desktop.txt
+node bin/index.js --batch scripts/batch/desktop-ie.txt
 node bin/index.js --batch scripts/batch/login-mobile.txt
 node bin/index.js --batch scripts/batch/login-desktop.txt
 node bin/index.js --batch scripts/batch/second-view-mobile.txt
 node bin/index.js --batch scripts/batch/second-view-desktop.txt
+node bin/index.js --batch scripts/batch/second-view-desktop-ie.txt
 node bin/index.js --batch scripts/batch/mobile-wpt-org.txt
 node bin/index.js --batch scripts/batch/desktop-wpt-org.txt
